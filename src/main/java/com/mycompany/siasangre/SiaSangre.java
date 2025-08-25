a719018d1709da1fee3be39bc2040cea6bd653f7
Mankeke <Mankeke@DESKTOP-LUL87I5>
2025-08-24 22:09:36 -0400

    Commit prueba

---
+++ src/main/java/com/mycompany/siasangre/SiaSangre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.siasangre;
+
+/**
+ *
+ * @author Mankeke
+ */
+public class SiaSangre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
