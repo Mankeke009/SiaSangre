7dd81d579ed6404c52c8a565b98b9ad0d5508e01
Mankeke <Mankeke@DESKTOP-LUL87I5>
2025-08-25 23:29:27 -0400

    Clase campaña lista

---
+++ src/main/java/com/mycompany/siasangre/Campana.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.siasangre;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Mankeke
+ */
+public class Campana {
+    private String nombreCampana;
+    private String lugar;
+    private List<Donante> donantesregistrados;
+
+    public Campana(String nombreCampana, String lugar, List<Donante> donantesregistrados) {
+        this.nombreCampana = nombreCampana;
+        this.lugar = lugar;
+        this.donantesregistrados = new ArrayList<>();
+    }
+
+    public String getNombreCampana() {
+        return nombreCampana;
+    }
+
+    public void setNombreCampana(String nombreCampana) {
+        this.nombreCampana = nombreCampana;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    
+    public List<Donante> getDonantesregistrados() {
+        return donantesregistrados;
+    }
+
+    public void setDonantesregistrados(List<Donante> donantesregistrados) {
+        this.donantesregistrados = donantesregistrados;
+    }
+  
+    public void AgregarDonante(Donante donante) {
+        this.donantesregistrados.add(donante);
+    }
+    public void  mostrarDonantes(){
+        System.out.printf("\n Donantes de la campaña %s.", this.nombreCampana);
+        if (donantesregistrados.isEmpty()){
+            System.out.println("No hay donantes registrados en la campaña");
+            
+        }
+        else{
+            for(Donante donante : donantesregistrados){
+                System.out.println(donante);
+            }
+        }
+    }
+    
+}
