5d5787a05acaead9a769c4727d20ba71faedc5e7
Mankeke <Mankeke@DESKTOP-LUL87I5>
2025-08-26 20:39:18 -0400

    NetBeans IDE 21

--- src/main/java/com/mycompany/siasangre/SistemaGestion.java
+++ src/main/java/com/mycompany/siasangre/SistemaGestion.java
@@ -1,5 +1,5 @@
 package com.mycompany.siasangre;
-
+//Commit NetBeans
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
